b6ef73ecab7d1222f06edd8e9fbf5d0a055f1412
CepVladislav <61963214+CepVladislav@users.noreply.github.com>
2020-10-12 10:10:38 +0300

    Add files via upload

---
+++ Ex5/Ex5.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <iomanip>
+
+class Array
+{
+private:
+    int* arr;
+    int N;
+public:
+    Array(int n)
+    {
+        std::cout << "Enter array limit: ";
+        std::cin >> n;
+        N = n;
+
+        arr = new int[n];
+        for (int i = 0; i < N; i++)
+        {
+            std::cout << "Enter elem number " << i+1 << " : ";
+            std::cin >> arr[i];
+        }
+    };
+    void Print()
+    {
+        std::cout << "| ";
+        for (int i = 0; i < N; i++)
+        {
+            std::cout << arr[i] << " | ";
+        }
+        std::cout << std::endl;
+    }
+    friend int DataSumm(Array &Array);
+    ~Array()
+    {
+        delete[]arr;
+    };
+};
+
+int DataSumm(Array &Array)
+{
+    int sum=NULL;
+    for (int i = 0; i < Array.N; i++) sum += Array.arr[i];
+    for (int i = 0; i < Array.N; i++) if (Array.arr[i] < 0) sum=NULL;
+    return sum;
+}
+
+int main()
+{
+    int n=0;
+    Array num1(n);
+    num1.Print();
+    std::cout << "Summ: " << DataSumm(num1) << std::endl;
+    return 0;
+}
