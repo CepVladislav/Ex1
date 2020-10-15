0e3bff1feb0672ab85298bb6d85711ec8846b3e9
CepVladislav <61963214+CepVladislav@users.noreply.github.com>
2020-10-15 10:07:05 +0300

    Add files via upload

---
+++ Cont1/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <array>
+
+class Employee
+{
+private:
+	std::string name;
+	long number;
+public:
+	void getdata();
+	void putdata();
+};
+
+void Employee::getdata()
+{
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cout << "Enter name: ";
+	getline(std::cin, name);
+	std::cout << "Enter number: ";
+	std::cin >> number;
+}
+
+void Employee::putdata()
+{
+	std::cout << "Name: " << name << " ";
+	std::cout << "Number: " << number << std::endl;
+}
+
+int main()
+{
+	std::array<Employee, 100> myarray;
+	for (auto& element : myarray) element.getdata();
+	for (auto& element : myarray) element.putdata();
+	return 0;
+}
