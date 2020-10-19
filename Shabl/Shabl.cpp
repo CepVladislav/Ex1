8ad77da21e6d80b68f58440c76f5a78f109eea86
CepVladislav <61963214+CepVladislav@users.noreply.github.com>
2020-10-19 10:51:06 +0300

    Add files via upload

--- Shabl/Shabl.cpp
+++ Shabl/Shabl.cpp
@@ -15,24 +15,28 @@ void func(T& a, T& b)
     std::cout << std::endl;
 }
 
-template<typename T>
-void funks(T* a, T* b)
+template<typename T, size_t N, size_t M>
+void funks(T (&a)[N],T (&b)[M] )
 {
     std::cout << "Enter string: ";
-    std::cin >> a >> b;
+    std::cin >> a;
+    std::cout << sizeof(a) << std::endl;
+    std::cin >> b;
+    std::cout << sizeof(b) << std::endl;
     if (fmod(sizeof(a), sizeof(b)) == 0)
     {
         strcat(a, b);
         std::cout << a;
     }
-    else if (fmod(sizeof(a), sizeof(b)) == 1) std::cout << (sizeof(a) > sizeof(b) ? a : b);
+    else if(fmod(sizeof(a), sizeof(b)) == 1) std::cout << (sizeof(a) > sizeof(b) ? a : b);
     else if (fmod(sizeof(a), sizeof(b)) == 2) std::cout << (sizeof(a) < sizeof(b) ? a : b);
     else if (fmod(sizeof(a), sizeof(b)) == 3)
     {
         int j=0;
         char counter[90];
-        for (int i = 0; i < (sizeof(a) > sizeof(b) ? sizeof(a) : sizeof(b)); i++)
+        for (int i = 0; i < sizeof(a); i++)
         {
+            if (a[i] == NULL) break;
             if (a[i] == b[i])
             {
                 counter[j] = a[i];
@@ -48,10 +52,9 @@ void funks(T* a, T* b)
 
 int main()
 {
-    float a, b;
-    func(a, b);
-    char c[90];
-    char d[90];
-    funks(c, d);
+    float c, d;
+    func(c, d);
+    char a[90], b[87];
+    funks(a, b);
     return 0;
 }
