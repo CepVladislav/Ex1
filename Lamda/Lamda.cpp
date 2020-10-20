22a3199ef2cc8907951de5071851d20bc217f3b3
CepVladislav <61963214+CepVladislav@users.noreply.github.com>
2020-10-20 10:48:46 +0300

    Add files via upload

---
+++ Lamda/Lamda.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+	int n;
+	std::vector<int> array;
+	size_t count = 0;
+	std::cout << "Enter array size: ";
+	std::cin >> n;
+	array.resize(n);
+	for (auto& const element : array)
+	{
+		std::cout << "Enter elem: ";
+		std::cin >> element;
+	}
+	sort(array.begin(), array.end(), [&count](int& a, int& b) 
+		{
+		++count;
+		return a < b;
+		});
+	std::cout << "Number of replases: " << count << std::endl;
+	std::cout << "Array: ";
+	for_each(array.begin(), array.end(), [](int _n)
+		{
+			std::cout << _n << " ";
+		});
+	std::cout << std::endl;
+
+
+	std::vector<std::string> arr{ "apple", "banana", "walnut", "lemon" };
+	std::string SearchName;
+	std::cout << "Enter key word: ";
+	std::cin >> SearchName;
+	auto found (find_if(arr.begin(), arr.end(), [SearchName](std::string s)
+		{
+			return s.find(SearchName) != std::string::npos;
+		}));
+	if (found == arr.end())
+	{
+		std::cout << "Nothing founded\n";
+	}
+	else
+	{
+		std::cout << "Found " << *found << '\n';
+	}
+	return 0;
+}
