383eb0122fa3ec5c229fc3416004e0b3271fa3b0
CepVladislav <61963214+CepVladislav@users.noreply.github.com>
2020-11-19 12:00:04 +0300

    Add files via upload

---
+++ exeption/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <array>
+#include <vector>
+
+int main()
+{
+    int a, b;
+	std::cout << "Enter 2 values: " << std::endl;
+    std::cin >> a >> b;
+	try
+	{
+		if (b == 0) throw 911;
+		std::cout << "Result: " << static_cast<double>(a)/b << std::endl;
+	}
+	catch (int c)
+	{
+		std::cout << "Error " << c << ": division by zero" << std::endl;
+	}
+    return 0;
+}
